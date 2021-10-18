0fbea938bcc038b9bce023eea37483670cf191d8
ThisALV <lel.charriere@orange.fr>
2021-10-18 14:21:55 +0200

    Doc for BooyahRangeView class

--- app/src/main/java/com/thisalv/getrange/BooyahRangeView.kt
+++ app/src/main/java/com/thisalv/getrange/BooyahRangeView.kt
@@ -5,6 +5,13 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import androidx.appcompat.widget.AppCompatTextView
 
+/**
+ * Displays basic data about Booyah % percent ranges for a specific character represented by its
+ * SSBU stock icon.
+ *
+ * @constructor Applies "{min}%-{max}%" to `TextView` text attribute, with min & max extracted from
+ * this element attributes, and applies a size of 30sp to this font.
+ */
 class BooyahRangeView constructor(
     context: Context, attrs: AttributeSet
 ): AppCompatTextView(context, attrs) {
@@ -12,13 +19,13 @@ class BooyahRangeView constructor(
         context.theme.obtainStyledAttributes(
             attrs, R.styleable.BooyahRangeView, 0, 0
         ).apply {
-            try {
+            try { // getInt() might fail if this TypedArray wasn't recycled by previous operation
                 val min = getInt(R.styleable.BooyahRangeView_min, 0)
                 val max = getInt(R.styleable.BooyahRangeView_max, 0)
 
                 this@BooyahRangeView.text = context.getString(R.string.range, min, max)
             } finally {
-                recycle()
+                recycle() // Required for next operations with TypedArray to run as expected
             }
         }
 
