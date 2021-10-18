934c8731fd9b1a60b9055cdf3e737df3ac7592dd
ThisALV <lel.charriere@orange.fr>
2021-10-18 17:39:13 +0200

    Fix: Uses setter for data binding var, and declares corresponding libraries

--- app/src/main/java/com/thisalv/getrange/BooyahRangeView.kt
+++ app/src/main/java/com/thisalv/getrange/BooyahRangeView.kt
@@ -9,26 +9,59 @@ import androidx.appcompat.widget.AppCompatTextView
  * Displays basic data about Booyah % percent ranges for a specific character represented by its
  * SSBU stock icon.
  *
- * @constructor Applies "{min}%-{max}%" to `TextView` text attribute, with min & max extracted from
- * this element attributes, and applies a size of 30sp to this font.
+ * @property min Left percentage to print on text
+ * @property max Right percentage to print on text
+ *
+ * @constructor Set font size to 30dp, and tries to read `min` and `max` from attributes then update
+ * displayed text
  */
 class BooyahRangeView constructor(
     context: Context, attrs: AttributeSet
 ): AppCompatTextView(context, attrs) {
+    private var min: Int = 0
+    private var max: Int = 0
+
     init {
         context.theme.obtainStyledAttributes(
             attrs, R.styleable.BooyahRangeView, 0, 0
         ).apply {
             try { // getInt() might fail if this TypedArray wasn't recycled by previous operation
-                val min = getInt(R.styleable.BooyahRangeView_min, 0)
-                val max = getInt(R.styleable.BooyahRangeView_max, 0)
+                min = getInt(R.styleable.BooyahRangeView_min, 0)
+                max = getInt(R.styleable.BooyahRangeView_max, 0)
 
-                this@BooyahRangeView.text = context.getString(R.string.range, min, max)
+                update()
             } finally {
                 recycle() // Required for next operations with TypedArray to run as expected
             }
         }
 
         setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
+    }
+
+    /**
+     * Applies "{min}%-{max}%" to `TextView` text attribute, with min & max extracted from
+     * this element attributes, and applies a size of 30sp to this font.
+     *
+     * @param min New left % to print in the range text
+     * @param max New right % to print in the range text
+     */
+    private fun update() {
+        this@BooyahRangeView.text = context.getString(R.string.range, min, max)
+    }
+
+    /**
+     * Updates `min`, then updates text and ask for redraw
+     */
+    fun setMin(min: Int) {
+        this.min = min // Ça fait Min-min ptdrrr
+        update()
+    }
+
+    /**
+     * Updates `max`, then updates text and ask for redraw
+     */
+    fun setMax(max: Int) {
+        this.max = max
+        update()
     }
 }
